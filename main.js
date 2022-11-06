fe071f9bb9990c1863b6ec2682af353849b84eb8
Carolina-nico <cs.uxuiti@gmail.com>
2022-11-06 13:23:39 -0300

    Finalização da NLW Copa.

--- main.js
+++ main.js
@@ -2,17 +2,20 @@
 function createGame(player1, hour, player2) {
   return `
   <li>
-    <img src="./assets/icon-${player1}.svg" alt="Bandeira do ${player1}">
+    <img src="./assets/icon-${player1}.svg" alt="Bandeira do ${player1}" title=${player1}>
     <strong>${hour}</strong>
-    <img src="./assets/icon-${player2}.svg" alt="Bandeira da ${player2}">
+    <img src="./assets/icon-${player2}.svg" alt="Bandeira da ${player2}" title=${player2}>
   </li>
   `
 }
 
+// variavel de controle
+let delay = 0;
 // funcão e (argumento da função)
 function createCard(date, day, games) {
+  delay = delay + 0.3;
   return `
- <div class="card">
+ <div class="card" style="animation-delay: ${delay}s">
     <h2>${date} <span>${day}</span></h2>
       <ul>
         ${games}
@@ -29,15 +32,15 @@ document.querySelector('#app').innerHTML = `
 
 <main id="cards">
   ${createCard('24/11', 'quinta',
-  createGame('switzerland','07:00','camaron') + createGame('uruguay','10:00','southkorea') + createGame('portugal','13:00','ghana') + createGame('brazil','16:00','serbia'))
+  createGame('switzerland','07:00','camaron') + createGame('uruguay','10:00','southkorea') + createGame('portugal','13:00','ghana') + createGame('brazil','16:00','servia'))
   }
 
   ${createCard('28/11', 'segunda',
-  createGame('camaron','07:00','serbia') + createGame('southkorea','10:00','ghana') + createGame('switzerland','13:00','brazil') + createGame('portugal','16:00','uruguay'))
+  createGame('camaron','07:00','servia') + createGame('southkorea','10:00','ghana') + createGame('switzerland','13:00','brazil') + createGame('portugal','16:00','uruguay'))
   }
 
   ${createCard('02/12', 'sexta',
- createGame('ghana','12:00','uruguay') + createGame('southkorea','12:00','portugal') + createGame('serbia','16:00','switzerland') + createGame('brazil','16:00','camaron'))
+ createGame('ghana','12:00','uruguay') + createGame('southkorea','12:00','portugal') + createGame('servia','16:00','switzerland') + createGame('brazil','16:00','camaron'))
   }
 </main>
 `
